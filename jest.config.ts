cba6457da51ccf569e6f3481877ac254a5a8c2c8
Bruno Bennotti <brunobennotti@gmail.com>
2022-06-02 22:28:47 -0300

    init

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/core/mocks/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
---
+++ src/app/tests/App.test.tsx
@@ -0,0 +1,18 @@
+// Imports
+import { render } from '@testing-library/react';
+
+// To Test
+import App from '../App';
+
+// Tests
+test('Renders main page correctly', () => {
+  // Setup
+  render(<App />);
+
+  // Pre Expecations
+
+  // Init
+
+  // Post Expectations
+  expect(true).toBeTruthy();
+});
